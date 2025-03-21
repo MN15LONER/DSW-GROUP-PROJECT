d55b09c9ecb1986bc070d73a8607e0c11f4ed2cf
Magayedev05 <manicforex22@gmail.com>
2025-03-21 14:07:34 +0200

    Update frontend.js

---
+++ frontend.js
@@ -0,0 +1,6 @@
+function validateForm() {
+    
+    const username = document.getElementById('username').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+}
