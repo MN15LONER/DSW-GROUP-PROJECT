3e69447e089f4118ead7dcb2ea9e5fb417247ac6
MN15LONER <mfunekondeya83@gmail.com>
2025-03-21 14:47:25 +0200

    Validating the input feilds

--- frontend.js
+++ frontend.js
@@ -1,6 +1,8 @@
-function validateForm() {
+let form  = document.getElementById("validateForm");
+
+form.addEventListener("submit", function validateForm() {
     
     const username = document.getElementById('username').value.trim();
     const email = document.getElementById('email').value.trim();
     const password = document.getElementById('password').value.trim();
-}
+})
