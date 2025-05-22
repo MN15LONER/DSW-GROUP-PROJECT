009d9ae8b318e4c6e9b9e6f9ce6150854352132d
Magayedev05 <manicforex22@gmail.com>
2025-05-22 12:35:51 +0200

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,9 @@
+document.getElementById('filter-btn').addEventListener('click', () => {
+    alert('filters applied!');
+});
+document.getElementById('export-btn').addEventListener('click', () => {
+    alert('Exported to CVS!');
+});
+document.getElementById('resolve-btn').addEventListener('click', () => {
+    alert('Report marked as resolved!');
+});
